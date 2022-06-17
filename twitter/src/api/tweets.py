9efda9df53b8c6fbed6831436b343490974eeefe
pokacyb <pokacyb@tutanota.com>
2022-06-17 23:19:04 +0200

    start working on api

---
+++ twitter/src/api/tweets.py
@@ -0,0 +1,4 @@
+from flask import Blueprint
+
+bp = Blueprint('tweets', __name__, url_prefix='/tweets')
+
---
+++ twitter/src/api/users.py
@@ -0,0 +1,4 @@
+from flask import Blueprint
+
+bp = Blueprint('users', __name__, url_prefix='/users')
+
