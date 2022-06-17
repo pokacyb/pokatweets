f330846b4068448dd0e193f39dec5182b0f8c01e
pokacyb <pokacyb@tutanota.com>
2022-06-17 22:32:18 +0200

    Create User model

---
+++ twitter/src/models.py
@@ -0,0 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+# Reference:
+# https://flask-sqlalchemy.palletsprojects.com/en/2.x/models/
+# https://docs.sqlalchemy.org/en/14/core/metadata.html#sqlalchemy.schema.Column
+# https://flask-sqlalchemy.palletsprojects.com/en/2.x/models/#many-to-many-relationships
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), unique=True, nullable=False)
+    pasword = db.Column(db.String(128), nullable=False)
+    
