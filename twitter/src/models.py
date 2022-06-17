2b5bde2ca9ec3b24c3b8fc2d1a3867e13b59ba95
pokacyb <pokacyb@tutanota.com>
2022-06-17 22:41:03 +0200

    Add Tweet model

--- twitter/src/models.py
+++ twitter/src/models.py
@@ -1,3 +1,4 @@
+import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -12,4 +13,14 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(128), unique=True, nullable=False)
     pasword = db.Column(db.String(128), nullable=False)
-    
+
+class Tweet(db.Model):
+    __tablename__ = 'tweets'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    content = db.Column(db.String(280), nullable=False)
+    created_at = db.Column(
+        db.DateTime,
+        default=datetime.datetime.utcnow,
+        nullable=False
+    )
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
